26d7c8a6a89765de7ef1265956f4e6d5abc34ad7
rafa <r.loboplaza@hotmail.com>
2022-06-16 09:13:38 -0300

    up

--- src/routes/users/src/restore-pass.js
+++ src/routes/users/src/restore-pass.js
@@ -12,6 +12,7 @@ async function restorePass(req,res){
     joivalidate(
         Joi.object({
             newPass: Joi.string().min(1).max(50).required(),
+            token:Joi.string().min(1).max(180).required()
         }),
         req.body,
         res,
@@ -19,8 +20,30 @@ async function restorePass(req,res){
             try {
                 const db = await mongo()
                 const verify = await jwt.verify(req.body.token, process.env.KEY_PRELOGIN)
-                
+                if(verify){
+                    const {email} = verify
+                    const user = await db.collection('users').findOne({email})
+                    if(!user){
+                        return res.status(401).json({
+                            msg: 'No tiene permisos para realizar esta acción, contacte al administrador.',
+                            code: 1
+                        })
+                    }else{
+                        const hashedPass = await bcrypt.hash(req.body.newPass, 10)
+                        await db.collection('users').updateOne({email}, {$set:{ password: hashedPass}})
+                        return res.status(200).json({
+                            message: "Contraseña actualizada exitosamente.",
+                            code: 2,
+                        });
+                    }
+                }else{
+                    return res.status(400).json({
+                        msg:'Usted no posee token de usuario, contacte al administrador.',
+                        code: 1
+                    })
+                }
             } catch (error) {
+                console.log('ress', error)
                 return res.status(500).json({
                     msg: 'Hubo un error en el servidor, intente nuevamente.',
                     code: -1
