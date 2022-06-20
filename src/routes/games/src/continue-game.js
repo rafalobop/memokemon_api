ffac16131cb0484d2e2f6741cf174532db148d54
rafa <r.loboplaza@hotmail.com>
2022-06-20 13:14:20 -0300

    ruta continuar juego actualizada

--- src/routes/games/src/continue-game.js
+++ src/routes/games/src/continue-game.js
@@ -8,18 +8,18 @@ router.get('/', validaToken, continueGame)
 async function continueGame(req,res){
     try {
         const db = await mongo()
-        const user = await db.collection('games').findOne({email: req.jwt.email})
-        const level = user.games.length
-        if(level === 0){
+        const user = await db.collection('users').findOne({email: req.jwt.email})
+        if(!user.gameProgress){
             return res.status(400).json({
                 msg:'No hay progreso iniciado en el juego. A jugar!',
                 code: 1
             })
+        }else{
+            return res.status(200).json({
+                level: user.gameProgress.levelActual,
+                code: 2
+            })
         }
-        return res.status(200).json({
-            level,
-            code: 2
-        })
     } catch (error) {
         return res.status(500).json({
             msg:'Hubo un error en el servidor, intente nuevamente.',
